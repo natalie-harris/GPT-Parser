0bdc8c62f5f460294f4ae77e6dc183b7e9193a2a
natalie <natalieharris.tn@gmail.com>
2024-02-11 15:46:38 -0500

    stopping point

--- tests/gptpl_test.py
+++ tests/gptpl_test.py
@@ -6,21 +6,36 @@ this file will help me build the primary functionality of the pipeline.
 It will import a bunch of text files containing book excerpts, it will read each excerpt and determine if it's appropriate to teach the excerpt to a 1st grade classroom
 """
 
+"""
+Notes:
+    First df is called "Files List"
+    Second df is called "Text List"
+"""
+
 from gptpipeline import *
-from datetime import datetime
+import os
 
-def Log_Date_and_Time(input_data):
-    now = datetime.now()
-    return "Received " + input_data + " at " + now.strftime("%Y-%m-%d %H:%M:%S")
+def replace_path_if_windows(file_path, old_part, new_part):
+    # Check if the current OS is Windows
+    print(os.name)
+    if os.name == 'nt':
+        # Replace the old part with the new part
+        new_file_path = file_path.replace(old_part, new_part)
+        return new_file_path
+    else:
+        # If not Windows, return the original path
+        return file_path
 
-with open("/Users/natalieharris/UTK/NIMBioS/GPTPipeline/openai_key.txt", "r") as fd:
+books_folder_path = replace_path_if_windows("/Users/natalieharris/UTK/NIMBioS/GPTPipeline/tests/corpus/books/", "/Users/natalieharris/UTK/NIMBioS/GPTPipeline/tests/corpus/books/", "E:\\NIMBioS\\GPT Parser\\tests\\corpus\\books\\")
+openai_key_path = replace_path_if_windows("/Users/natalieharris/UTK/NIMBioS/GPTPipeline/openai_key.txt", "/Users/natalieharris/UTK/NIMBioS/GPTPipeline/", "E:\\NIMBioS\\GPT Parser\\")
+
+with open(openai_key_path, "r") as fd:
     api_key = fd.read()
-books_folder_path = "/Users/natalieharris/UTK/NIMBioS/GPTPipeline/tests/corpus/books"
 
 pipeline = GPTPipeline(api_key)
-generate_primary_csv(books_folder_path, "ebooks.csv", books_folder_path, **{})
-pipeline.import_texts("/Users/natalieharris/UTK/NIMBioS/GPTPipeline/tests/corpus/books/ebooks.csv", 100)
+generate_primary_csv(books_folder_path, "ebooks_updated.csv", books_folder_path, **{})
+pipeline.import_texts(books_folder_path + "ebooks_updated.csv", 100)
 pipeline.process("Placeholder Data", 100)
 
 # pipeline.print_modules()
-# pipeline.print_dfs()
+pipeline.print_text_df()
